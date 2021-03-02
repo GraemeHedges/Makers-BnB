cc9d7c2ce2ae37273a80780b6bb639bb62ee46e5
mitchell9-1 <mitchell.perry001@gmail.com>
2021-03-02 16:13:29 +0000

    Correctly removed the Config within the application controllers.

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -2,6 +2,10 @@ require_relative './application_controller'
 
 class SignUp < ApplicationController
 
+  get '/sign-up' do
+    erb(:sign_up)
+  end
+  
   post '/sign-up' do
     redirect '/'
   end
