dba9ee877e852b9a1b4dca8bfcbc891a8c815837
Harry Thomson <haroldt95@hotmail.com>
2021-03-04 16:36:07 +0000

    first test complete

--- spec/spaces_model_spec.rb
+++ spec/spaces_model_spec.rb
@@ -4,15 +4,9 @@ describe Spaces do
 
   params = {:name=>"A Boat on the Sea", :from=>"2021-03-01", :to=>"2021-03-03", :description=>"cosy boat with lots of baileys and shoes", :price=>"70.00", :space_owner=>"Old Gregg"}
 
-  it 'creates a database entry' do
+  it 'searches what properties are available on a certain date' do
     Spaces.create(params)
-    space = Spaces.search("2021-04-01").map { |t| [[t.name], [t.description], [t]]}
-    p '******************'
-    p Spaces.search("2021-04-01").description
-    p space
-    p '******************'
-
-    expect(space).to include('A Boat on the Sea')
+    expect(Spaces.search("2021-04-01")[0].name).to eq('A Boat on the Sea')
   end
 
 end
