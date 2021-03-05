a98e06d76d9dad2afface65ceee79a1dcfc13f7f
AlistairTait12 <alistair.tait1@gmail.com>
2021-03-05 12:20:44 +0000

    created sign_up_user web helper

--- spec/Features/sign_up_spec.rb
+++ spec/Features/sign_up_spec.rb
@@ -12,22 +12,12 @@ feature "user has option to sign up" do
   end
 
   scenario "fill in form and submit redirects to homepage" do
-    visit('/sign-up')
-    fill_in('name', with: 'Old Greg')
-    fill_in('email', with: 'oldgregg@greggs.co.ugreggs')
-    fill_in('password', with: 'greggypass')
-    fill_in('confirm-password', with: 'greggypass')
-    click_button "Submit"
+    sign_up_user
     expect(page).to have_link "Sign in", href: "/sign-in"
   end
 
   scenario "fill in form and submit adds user to database" do
-    visit('/sign-up')
-    fill_in('name', with: 'Old Greg')
-    fill_in('email', with: 'oldgregg@greggs.co.ugreggs')
-    fill_in('password', with: 'greggypass')
-    fill_in('confirm-password', with: 'greggypass')
-    click_button "Submit"
+    sign_up_user
     user = User.find_by(email: 'oldgregg@greggs.co.ugreggs')
     expect(user.name).to eq('Old Greg')
   end
--- spec/Features/web_helpers.rb
+++ spec/Features/web_helpers.rb
@@ -6,3 +6,12 @@ def sign_in_user
   fill_in 'password', with: 'Password1!'
   click_on 'Submit'
 end
+
+def sign_up_user
+  visit('/sign-up')
+  fill_in('name', with: 'Old Greg')
+  fill_in('email', with: 'oldgregg@greggs.co.ugreggs')
+  fill_in('password', with: 'greggypass')
+  fill_in('confirm-password', with: 'greggypass')
+  click_button "Submit"
+end
